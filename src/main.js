bb09c061ca078f4653c82305352dae6202289af5
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-03-24 15:14:32 +0200

    behavior

--- src/main.js
+++ src/main.js
@@ -13,8 +13,10 @@ searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const query = event.currentTarget.elements['search-text'].value.trim();
 
-  if (query === "") return;
-
+  if (query === "") {
+    iziToast.warning({ message: "Please enter a search query!" });
+    return;
+  }
 
   currentQuery = query;
   page = 1;         
@@ -26,19 +28,26 @@ searchForm.addEventListener('submit', async (event) => {
     const data = await getImagesByQuery(currentQuery, page);
     
     if (data.hits.length === 0) {
-      iziToast.error({ message: 'No images found!' });
+      iziToast.error({ message: 'Sorry, there are no images matching your search query. Please try again!' });
       return;
     }
 
     createGallery(data.hits); 
-    
 
-    if (data.totalHits > perPage) {
+
+    const totalPages = Math.ceil(data.totalHits / perPage);
+
+    if (page >= totalPages) {
+      
+      hideLoadMoreButton();
+      iziToast.info({ message: "We're sorry, but you've reached the end of search results." });
+    } else {
       showLoadMoreButton();
     }
+
   } catch (error) {
     console.error(error);
-    iziToast.error({ message: 'Something went wrong. Please try again later.' });
+    iziToast.error({ message: 'An error occurred while fetching images. Please try again later.' });
   } finally {
     hideLoader();
   }
@@ -54,11 +63,11 @@ loadMoreBtn.addEventListener('click', async () => {
     
     createGallery(data.hits);
     
-
+  
     smoothScroll();
 
-
     const totalPages = Math.ceil(data.totalHits / perPage);
+
     if (page >= totalPages) {
       hideLoadMoreButton();
       iziToast.info({ message: "We're sorry, but you've reached the end of search results." });
@@ -67,7 +76,7 @@ loadMoreBtn.addEventListener('click', async () => {
     }
   } catch (error) {
     console.error(error);
-    iziToast.error({ message: 'Failed to load more images.' });
+    iziToast.error({ message: 'Failed to load more images. Check your connection.' });
   } finally {
     hideLoader();
   }
@@ -79,6 +88,7 @@ function smoothScroll() {
     const cardHeight = card.getBoundingClientRect().height;
     window.scrollBy({
       top: cardHeight * 2,
+      behavior: 'smooth', 
     });
   }
 }
